1eafcb1d16062dea31a65a269c6bbee1c9a41f47
Sammy593 <samuelto1359@gmail.com>
2022-05-31 09:02:35 -0500

    se quita form de agregar y añade controlador reg

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ from flask import Flask, redirect, request, render_template, url_for
 app = Flask(__name__, template_folder='templates')
 
 
-cliente = []
+registrado = []
+""""
+class Cliente:
+    # Constructor de clase
+    def __init__(self, nombre, telefono, estado):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.estado = estado
+        print('Se ha creado cliente:',self.nombre)
+    def __str__(self):
+        return '{} ({})'.format(self.nombre, self.telefono, self.estado)
+
+"""
 @app.route('/')
 #contenedor para llamar a index.html
 def index():
-    return render_template('/index.html')
+    return render_template('/index.html', registrado = registrado)
 
 @app.route('/sesion')
 def sesion():
@@ -21,17 +33,22 @@ def registro():
      
 
 @app.route('/registrarC', methods =["GET", "POST"])
-#contenedor para llamar a enviar.html
+#contenedor
 def registrarC():
     if request.method == 'POST':
-        tituloT = request.form['titulo']
-        correoT = request.form['correo']
-        prioridadT = request.form['prioridad']
-
-
-        cliente.append({'titulo': tituloT,'correo': correoT,'prioridad': prioridadT})
-   
         return redirect(url_for('index'))
+
+
+
+#contenedor para agregar registro
+@app.route('/regcliente_tienda', methods =["GET", "POST"])
+def regcliente_tienda():
+    if request.method == 'POST':
+        Cnombre = request.form['cliente']
+        Tnombre = request.form['tienda']
+        registrado.append({'cliente': Cnombre,'tienda': Tnombre})
+        return redirect(url_for('index'))
+        
 
 #ejecutar
 if __name__ == '__main__':
